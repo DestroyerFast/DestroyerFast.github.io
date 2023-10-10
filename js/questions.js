2d76be3820d519068afc7cf9ce79582f95c5224f
erdevelo <erdevelop13@gmail.com>
2023-10-09 19:38:31 -0500

    Pagina Marina

---
+++ js/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "¿Qué debes de hacer en un caso de tiroteo en una carretera o en cualquier otro lugar?",
+    answer:
+      "Actuar y llamar a refuerzos con mi 10-20, y si no llegán debo de actuar con disparos a dichos criminales",
+    options: [
+      "Correr, Esconderse e Huir",
+      "Actuar y llamar a refuerzos con mi 10-20, y si no llegán debo de actuar con disparos a dichos criminales",
+      "Protegerme con el carro",
+      "Huir con las demás personas y no salvar a civiles",
+    ],
+  },
+  {
+    numb: 2,
+    question: "¿A que se dedica/Encarga la marina actualmente?",
+    answer: "maneja operaciones en mar, aire y tierra",
+    options: [
+      "maneja operaciones en mar, aire y tierra",
+      "Disparar, Arrestar a criminales más buscado de México",
+      "Realizar operaciones en mar y aire junto a Sedena",
+      "Garantizar la seguridad del país e cuidar a civiles dentro de las calles, patrullar noche y dia",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      "Si encontramos que eres infiltrado o haces cosas ilegales que vayan en contra de la marina armada, no te preguntaremos y te sacaremos al instante al igual que se te colocara en la lista de enemigos del estado y serás buscado por todas las leyes. ¿Estás de acuerdo con eso?",
+    answer: "si",
+    options: ["si", "no", "lo dudo", "tal vez"],
+  },
+  {
+    numb: 4,
+    question: "¿Qué protege actualmente la Marina?",
+    answer: "operaciones de búsqueda, rescate",
+    options: [
+      "operaciones de búsqueda, rescate",
+      "Proteger los intereses marítimos nacionales, mediante la sustitución, adquisición",
+      " la creación y equipamiento de unidades de Infantería de Marina.",
+      "Otro",
+    ],
+  },
+  {
+    numb: 5,
+    question: "¿Qué acciones realiza la Marina?",
+    answer: "Velar por el bienestar de la población e país",
+    options: [
+      "Vigilar y apoyar al Gobierno sin necesidad alguna",
+      "Velar por el bienestar de la población e país",
+      "Violar los Derechos soberanos",
+      "Entrenar a los marinos, dar capacitación, etc",
+    ],
+  },
+  {
+    numb: 6,
+    question: "¿Qué ofrece la Semar?",
+    answer:
+      "Generar estrategias para la seguridad y protección de civiles, nacionales e internacionales",
+    options: [
+      "Asesorar en la toma de decisiones inherentes a la administración marítima y portuaria",
+      "Generar estrategias para la seguridad y protección de civiles, nacionales e internacionales",
+      "Tener poder jurídico",
+      "otros",
+    ],
+  },
+  {
+    numb: 7,
+    question: "¿Qué controla la marina de méxico?",
+    answer:
+      "Emplear la Federación para la defensa exterior y coadyuvar en la seguridad interior del país",
+    options: [
+      "Controla el nivel del mar de las fuerzas especiales navales",
+      "Emplear la Federación para la defensa exterior y coadyuvar en la seguridad interior del país",
+      "Encargarse de la Agencia Federal de Aviación Civil (AFAC)",
+      "Servicios aeroportuarios al nivel mundial",
+    ],
+  },
+  {
+    numb: 8,
+    question: "¿Qué hace un Cabo de la Marina?",
+    answer:
+      "Respetar el orden constitucional y evitar su empleo por el narcotráfico y la delincuencia organizada",
+    options: [
+      "Reciben entrenamientos avanzados, personalizados por sus superiores",
+      "Hacer negocios ilegales dentro de la facción policial",
+      "Ser Eficiente",
+      "Respetar el orden constitucional y evitar su empleo por el narcotráfico y la delincuencia organizada",
+    ],
+  },
+  {
+    numb: 9,
+    question: "¿Qué habilidades debe tener un marino?",
+    answer:
+      "Ser ordenado, disciplinado y tolerante e acatar las ordenes de sus superiores/Altos Mandos",
+    options: [
+      "Conocimientos de navegación y de radiocomunicaciones",
+      "Conocimientos de electrónico y mecánica",
+      "Ser ordenado, disciplinado y tolerante e acatar las ordenes de sus superiores/Altos Mandos",
+      "cumplimiento de los deberes ilícitos",
+    ],
+  },
+  {
+    numb: 10,
+    question: "¿Qué valores debe tener un marino?",
+    answer:
+      "la obediencia y un alto concepto del honor, la justicia y la moral",
+    options: [
+      "la obediencia y un alto concepto del honor, la justicia y la moral",
+      "El exCumplimiento de sus superiores",
+      "Tener un valor ilícito",
+      "No respetar el Gobierno/La Ley",
+    ],
+  },
+];
